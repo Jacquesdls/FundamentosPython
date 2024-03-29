2900e35ede16db74398c81d735189be6092b1db4
adrianodomingues <adrianodomingues@hotmail.com>
2024-03-29 18:09:14 -0300

    listas

---
+++ aula56a.py
@@ -0,0 +1,16 @@
+"""  
+    split e join com list e str
+    split - divide um string
+    join - une uma string
+"""
+
+frase = '   Olha só que    ,   coisa interessante            '
+lista_frases_cruas = frase.split(',')
+print(lista_frases_cruas)
+
+lista_frases = []
+
+for i, frase in enumerate(lista_frases_cruas):
+   lista_frases.append(lista_frases_cruas[i].strip())
+   
+print(lista_frases)
